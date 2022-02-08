2ba8f3a859de570d0a9850fa5ff332f67b4deca7
inazhurba <inazhurba@gmail.com>
2022-02-09 00:16:30 +0200

    main with parameters

--- main.cpp
+++ main.cpp
@@ -7,11 +7,29 @@
 #include <sstream>
 #include <fstream>
 #include <vector>
-#include <boost/lexical_cast.hpp>//до того ж методу що ще не працює
+#include <chrono>
+#include <atomic>
+//#include <boost/lexical_cast.hpp>//до того ж методу що ще не працює
 
 std::string file1, file2;
+long long full_execution_time = 0;
+
+inline std::chrono::high_resolution_clock::time_point get_current_time_fenced()
+{
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    auto res_time = std::chrono::high_resolution_clock::now();
+    std::atomic_thread_fence(std::memory_order_seq_cst);
+    return res_time;
+}
+
+template<class D>
+inline long long to_us(const D& d)
+{
+    return std::chrono::duration_cast<std::chrono::microseconds>(d).count();
+}
 
 int method_1(double d){
+    auto start_time = get_current_time_fenced();
     // Including header file sstream is a must.
     // For large numbers, ostringstream automatically
     // converts it to scientific notation
@@ -23,24 +41,39 @@ int method_1(double d){
     //num<<d;
     std::string str = num.str();
 
-    //std::cout << str << std::endl;
-    return str.length();
+    auto finish_time = get_current_time_fenced();
+
+    auto total_time = finish_time - start_time;
+    full_execution_time += to_us(total_time);
+
+    return (int)str.length();
 }
 int method_2(double d){
+    auto start_time = get_current_time_fenced();
     //It has a default precision of six
     // digits which cannot be changed
     std::string str = std::to_string(d);
 
-    //std::cout<< str<<std::endl;
-    return str.length();
+    auto finish_time = get_current_time_fenced();
+
+    auto total_time = finish_time - start_time;
+    full_execution_time += to_us(total_time);
+
+    return (int)str.length();
 }
 //ще не працює, розбираюсь ще з ним
 int method_3(double d){
+    auto start_time = get_current_time_fenced();
     std::string str;
-    str = std::lexical_cast<std::string>(d);
+    //str = std::lexical_cast<std::string>(d);
+
+    auto finish_time = get_current_time_fenced();
+
+    auto total_time = finish_time - start_time;
+    full_execution_time += to_us(total_time);
 
     std::cout<< str<<std::endl;
-    return str.length();
+    return (int)str.length();
 }
 
 void write_file(int amount, double average){
@@ -65,7 +98,7 @@ void write_file(int amount, double average){
 
 void pre_method(int n, const std::vector<double>& data){
     int characters_amount = 0;
-    int data_size = data.size();
+    int data_size = (int)data.size();
     switch(n) {
         case 1:
             for (double d : data) {
@@ -106,14 +139,28 @@ std::vector<double> read_file(){//(const std::string& file){
     return data;
 }
 
-int main() {
-    int n;
+int main(int argc, char* argv[]) {
 
-    std::cin>>n>>file1>>file2;
+    int n;
+    if(argc>1){
+        //std::cout<<argv[1]<<std::endl;
+        //std::cout<<argv[2]<<std::endl;
+        //std::cout<<argv[3]<<std::endl;
+        n = atoi(argv[1]);
+        file1 = argv[2];
+        file2 = argv[3];
+    }
+    else{
+
+        std::cin>>n>>file1>>file2;
+
+    }
 
     std::vector<double> file_data = read_file();
 
     pre_method(n,file_data);
 
+    //туту буде вивід виміряного часу
+    std::cout<<full_execution_time<<std::endl;
     return 0;
 }
